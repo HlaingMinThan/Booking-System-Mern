dfc14b711b02b7d6e3ba4b47e2be3b0a89e0897e
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-02-20 23:20:32 +0630

    axios setup and register form submmision

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
-
+import axios from 'axios'
+axios.defaults.baseURL = "http://localhost:4000"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Login() {
+
+    let [username, setUsername] = useState('');
+    let [email, setEmail] = useState('');
+    let [password, setPassword] = useState('');
+
+    let register = (e) => {
+        e.preventDefault();
+        axios.post('/register', {
+            username, email, password
+        });
+    }
+
     return (
         <div className='min-h-screen flex items-center justify-around'>
-            <div className='max-w-2xl mx-auto -mt-48 p-5'>
+            <form className='max-w-2xl mx-auto -mt-48 p-5' onSubmit={register}>
                 <h1 className='text-3xl font-bold text-center'>Register</h1>
-                <input type="text" placeholder='John Doe' />
-                <input type="text" placeholder='your@email.com' />
-                <input type="password" placeholder='password' />
+                <input type="text" placeholder='John Doe' value={username} onChange={e => setUsername(e.target.value)} />
+                <input type="text" placeholder='your@email.com' value={email} onChange={e => setEmail(e.target.value)} />
+                <input type="password" placeholder='password' value={password} onChange={e => setPassword(e.target.value)} />
                 <button className='primary'>Register now</button>
                 <div className='text-gray-500 my-2 text-center'>
                     Already have an account ? <Link to="/login" className="text-black">Login here</Link>
                 </div>
-            </div>
+            </form>
         </div>
     )
 }
