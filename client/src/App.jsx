1ed135f380f66fdee63cd0524f23ab1a5ebd0051
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-02-20 22:21:24 +0630

    react router setup done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       <Navbar />
+      <Outlet />
     </div>
   )
 }
