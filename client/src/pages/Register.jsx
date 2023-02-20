258d0dd4a3aa03a92400618e1ccbfe6ad1ed5aec
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-02-20 22:51:59 +0630

    add username input

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -6,6 +6,7 @@ export default function Login() {
         <div className='min-h-screen flex items-center justify-around'>
             <div className='max-w-2xl mx-auto -mt-48 p-5'>
                 <h1 className='text-3xl font-bold text-center'>Register</h1>
+                <input type="text" placeholder='John Doe' />
                 <input type="text" placeholder='your@email.com' />
                 <input type="password" placeholder='password' />
                 <button className='primary'>Register now</button>
