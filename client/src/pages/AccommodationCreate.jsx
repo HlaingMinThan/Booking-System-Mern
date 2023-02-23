501f93d062d15bb732e22b981fa352815e8aeb28
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-02-23 23:19:53 +0630

    ui update

--- client/src/pages/AccommodationCreate.jsx
+++ client/src/pages/AccommodationCreate.jsx
@@ -43,15 +43,15 @@ export default function AccommodationCreate() {
                     <div>
 
                         <div className="grid grid-cols-2 gap-3 md:grid-cols-4">
-                            <button className='flex justify-center items-center px-4 gap-1 border w-[120px] h-[100px] rounded-lg text-gray-400'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
-                            </svg>Upload
-                            </button>
                             {!!photos.length && photos.map(photo => (
                                 <div>
                                     <img src={photo} width="100" />
                                 </div>
                             ))}
+                            <button className='flex justify-center items-center px-4 gap-1 border w-[120px] h-[100px] rounded-lg text-gray-400'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
+                            </svg>Upload
+                            </button>
                         </div>
                     </div>
                 </div>
