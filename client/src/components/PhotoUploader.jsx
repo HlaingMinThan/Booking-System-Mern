168457e97f405ed8a9a290117c1959a89aaf8a3a
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-02-25 20:30:53 +0630

    handle delete photo

--- client/src/components/PhotoUploader.jsx
+++ client/src/components/PhotoUploader.jsx
@@ -32,6 +32,9 @@ export default function PhotoUploader({ photos, setPhotos }) {
         let urls = resPhotos.map(p => p.url);
         setPhotos(prev => [...prev, ...urls]);
     }
+    let handlePhotoDelete = (photo) => {
+        setPhotos(prev => prev.filter(p => p !== photo));
+    }
     return (
         <div className="mb-3">
             <Label label="Photos" description="more = better" />
@@ -45,8 +48,12 @@ export default function PhotoUploader({ photos, setPhotos }) {
 
                     <div className="grid grid-cols-2 gap-3 md:grid-cols-4">
                         {!!photos.length && photos.map(photo => (
-                            <div key={photo}>
-                                <img src={photo} width="100" />
+                            <div key={photo} className='h-32 w-32 relative bg-gray-100'>
+                                <img src={photo} className='object-cover' />
+
+                                <svg onClick={() => handlePhotoDelete(photo)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 absolute right-2 bottom-2 bg-gray-400 rounded-lg text-white bg-opacity-50 p-1 cursor-pointer">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                </svg>
                             </div>
                         ))}
                         {/* trick to make custom appearance of input file 🎉 */}
