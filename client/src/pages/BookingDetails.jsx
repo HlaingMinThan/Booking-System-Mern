e3be498cf1cddc2197af53508393e33d4f44cf39
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-02-26 18:53:13 +0630

    boooking details info added

--- client/src/pages/BookingDetails.jsx
+++ client/src/pages/BookingDetails.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import PlaceInfo from '../components/PlaceInfo';
 import PhotosGallery from '../components/PhotosGallery';
 import axios from 'axios';
+import PrettyDateBetween from '../components/PrettyDateBetween';
 
 export default function BookingDetails() {
     let { id } = useParams();
@@ -19,8 +20,23 @@ export default function BookingDetails() {
             <>
                 <PlaceInfo place={booking.place} />
                 {/* show booking detail */}
-                <div>
+                <div className='flex justify-between items-center bg-gray-200 rounded-xl p-6 my-12'>
+                    <div>
+                        <h2 className='text-xl'>Your Booking Information:</h2>
+                        <div className='flex gap-4 my-5'>
+                            <p className='flex gap-2'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+                                </svg>
 
+                                {booking.price / booking.place.price} nights</p>
+                            <PrettyDateBetween start={booking.checkIn} end={booking.checkOut} />
+                        </div>
+                    </div>
+                    <div className='bg-primary p-6  rounded-2xl text-white'>
+                        <h2 className='text-xl'>Total Price</h2>
+                        <h1 className='text-2xl'>${booking.price}</h1>
+                    </div>
                 </div>
                 <PhotosGallery place={booking.place} />
             </>
--- client/src/pages/Bookings.jsx
+++ client/src/pages/Bookings.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
+import PrettyDateBetween from '../components/PrettyDateBetween';
 
 export default function Bookings() {
     let [bookings, setBookings] = useState([]);
@@ -19,19 +19,7 @@ export default function Bookings() {
                     </div >
                     <div className='shrink grow-0 space-y-2 space-x-2'>
                         <h2 className='text-xl'>{booking.place.title}</h2>
-                        <p className='flex gap-2 items-center'>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
-                            </svg>
-                            {format(new Date(booking.checkIn), 'yyyy-MM-dd')}
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
-                            </svg>
-                            {format(new Date(booking.checkOut), 'yyyy-MM-dd')}
-                        </p>
+                        <PrettyDateBetween start={booking.checkIn} end={booking.checkOut} />
                         <p>Total Price - <span className="bg-primary text-white px-2 py-1 rounded-2xl">$ {booking.price}</span></p>
                         <p>Total Nights - {booking.price / booking.place.price} nights</p>
                     </div>
